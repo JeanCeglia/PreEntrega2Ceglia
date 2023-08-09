ffbe2ee7c96a91acb23e52562fb846b051b2b583
Jean Franco Ceglia <gianfc1018@gmail.com>
2023-08-09 19:01:25 -0300

    preEntrega 2

---
+++ js/main.js
@@ -0,0 +1,121 @@
+const ciudadesPorProvincia = {
+  "Buenos Aires": buenosAires,
+  "Chubut": chubut,
+  "Cordoba": cordoba,
+  "Entre Rios": entreRios,
+  "La Pampa": laPampa,
+  "La Rioja": laRioja,
+  "Mendoza": mendoza,
+  "San Juan": sanJuan,
+  "San Luis": sanLuis,
+  "Santa Cruz": santaCruz,
+  "Santiago del Estero": santiagoDelEstero,
+  "Tierra del Fuego": tierraDelFuego,
+};
+
+let prov = document.getElementById("provincias");
+let search = document.getElementById("search");
+
+search.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let selectOpt = prov.options[prov.selectedIndex];
+  let modal = document.createElement("div");
+  modal.className = "modal";
+  document.body.appendChild(modal);
+
+  let modalDialog = document.createElement("div");
+  modalDialog.className = "modal-dialog";
+  modal.appendChild(modalDialog);
+
+  let modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  modalDialog.appendChild(modalContent);
+
+  let modalHeader = document.createElement("div");
+  modalHeader.className = "modal-header";
+  modalContent.appendChild(modalHeader);
+
+  let modalH2 = document.createElement("h2");
+  modalH2.className = "modal-title";
+  modalH2.innerText = `${selectOpt.value}`;
+  modalHeader.appendChild(modalH2);
+
+  let btnModal = document.createElement("button");
+  btnModal.className = "btn-class";
+  btnModal.setAttribute("data-bs-dismiss", "modal");
+  btnModal.setAttribute("aria-label", "Close");
+  btnModal.innerText = "Cerrar";
+  modalHeader.appendChild(btnModal);
+
+  let modalBody = document.createElement("div");
+  modalBody.className = "modal-body";
+  modalContent.appendChild(modalBody);
+  if (selectOpt.value) {
+    let inputlugares = document.createElement("input");
+    inputlugares.setAttribute("type", "text");
+    inputlugares.setAttribute("id", "provCordoba");
+    modalBody.appendChild(inputlugares);
+    let lugares = document.getElementById("provCordoba");
+
+    let searchButton = document.createElement("button");
+    searchButton.className = "btn btn-primary";
+    searchButton.innerText = "Buscar Ciudad";
+    modalBody.appendChild(searchButton);
+
+    let verButton = document.createElement("button");
+    verButton.className = "btn btn-primary";
+    verButton.innerText = "mostrar Ciudad";
+    modalBody.appendChild(verButton);
+
+
+    /* Evento mostrar todas las ciudades disponibles */
+    verButton.addEventListener('click', () => {
+        let verCiudades = '';
+        if(selectOpt.value === 'Cordoba'){  
+          cordoba.forEach((ciudades) => {
+            verCiudades += `Ciudade de: ${ciudades.ciudad}, Codigo postal: ${ciudades.postalCode} <br>`
+          })
+          modalBody.innerHTML = verCiudades;
+        }else{
+          modalBody.innerText = 'En construccion'
+        }
+    })
+
+
+    /* Evemto buscar ciudad especifica */
+    searchButton.addEventListener("click", () => {
+
+      let ciudadBuscada = lugares.value;
+      let ciudadEncontrada;
+
+      if (ciudadesPorProvincia.hasOwnProperty(selectOpt.value)) {
+        const provinciaSeleccionada = selectOpt.value;
+        const ciudadesProvincia = ciudadesPorProvincia[provinciaSeleccionada];
+        ciudadEncontrada = ciudadesProvincia.find(
+          (ciudad) =>
+            ciudad.ciudad.toLowerCase() === ciudadBuscada.toLowerCase()
+        );
+
+        if (ciudadEncontrada) {
+          modalBody.innerHTML = `<p>Ciudad: ${ciudadEncontrada.ciudad}</p>`;
+          modalBody.innerHTML += `<p>Código Postal: ${ciudadEncontrada.postalCode}</p>`;
+        } else {
+          modalBody.innerHTML = "<p>Ciudad no encontrada.</p>";
+        }
+      } else {
+        modalBody.innerHTML = "<p>Provincia no encontrada.</p>";
+      }
+    });
+  }
+
+  // Mostrar el modal utilizando Bootstrap
+  let bootstrapModal = new bootstrap.Modal(modal);
+  bootstrapModal.show();
+});
+
+/* Esta es una seccion de mi proyecto, en el cual lo que busco es tener un buscador interactivo de todos los lugares donde tenemos un lugar de atencion
+
+como es obvio, le falta mucho, pero para no alargar la entrega, lo subo con puntos que, hasta donde entiendo, cumple las consignas
+
+*/
---
+++ js/myObject.js
@@ -0,0 +1,94 @@
+// Objetos de lugares de atención -----------
+
+const buenosAires = [{ ciudad: "America", postalCode: "6023" }];
+
+const chubut = [{ ciudad: "Comodoro Rivadavia", postalCode: "9000" }];
+
+const cordoba = [
+  { ciudad: "Argüello", postalCode: "5147" },
+  { ciudad: "Alcira Gigena", postalCode: "5809" },
+  { ciudad: "Alta Gracia", postalCode: "5186" },
+  { ciudad: "Balnearia", postalCode: "5942" },
+  { ciudad: "Berrotaran", postalCode: "5913" },
+  { ciudad: "Brinkmann", postalCode: "2419" },
+  { ciudad: "Córdoba", postalCode: "5000" },
+  { ciudad: "Coronel Moldes", postalCode: "5801" },
+  { ciudad: "Despeñaderos", postalCode: "5185" },
+  { ciudad: "Freyre", postalCode: "2410" },
+  { ciudad: "Holmberg", postalCode: "5987" },
+  { ciudad: "Jesús María", postalCode: "5220" },
+  { ciudad: "La puerta", postalCode: "5961" },
+  { ciudad: "Mina Clavero", postalCode: "5889" },
+  { ciudad: "Morteros", postalCode: "2421" },
+  { ciudad: "Oliva", postalCode: "5980" },
+  { ciudad: "Oncativo", postalCode: "5986" },
+  { ciudad: "Pilar", postalCode: "5282" },
+  { ciudad: "Rio ceballos", postalCode: "5111" },
+  { ciudad: "Rio cuarto", postalCode: "5800" },
+  { ciudad: "Sampacho", postalCode: "2650" },
+  { ciudad: "San Basilio", postalCode: "5933" },
+  { ciudad: "San Francisco", postalCode: "2400" },
+  { ciudad: "Santa Rosa De Calamuchita", postalCode: "5196" },
+  { ciudad: "Vicuña Mackena", postalCode: "6121" },
+  { ciudad: "Villa Allende", postalCode: "5105" },
+  { ciudad: "Villa Carlos paz", postalCode: "5152" },
+  { ciudad: "Villa General Belgrano", postalCode: "5194" },
+  { ciudad: "Villa Santa Rosa", postalCode: "5976" },
+  { ciudad: "Villa Del Totoral", postalCode: "5236" },
+  { ciudad: "Villa Dolores", postalCode: "5870" },
+];
+
+const entreRios = [
+  { ciudad: "Concordia", postalCode: "3200" },
+  { ciudad: "Federación", postalCode: "3206" },
+  { ciudad: "General Galarza", postalCode: "3172" },
+  { ciudad: "Gualeguay", postalCode: "2820" },
+  { ciudad: "Nogoyá", postalCode: "3150" },
+  { ciudad: "San Salvador", postalCode: "3196" },
+  { ciudad: "Villa Elisa", postalCode: "3265" },
+  { ciudad: "Urdinarrain", postalCode: "2822" },
+];
+
+const laPampa = [
+  { ciudad: "General Pico", postalCode: "6360" },
+  { ciudad: "Intendente Alvear", postalCode: "6385" },
+  { ciudad: "Realicó", postalCode: "6200" },
+  { ciudad: "Santa Rosa", postalCode: "6300" },
+];
+
+const laRioja = [{ ciudad: "La Rioja", postalCode: "5300" }];
+
+const mendoza = [
+  { ciudad: "General Alvear", postalCode: "5620" },
+  { ciudad: "San Rafael", postalCode: "5600" },
+];
+
+const sanJuan = [
+  { ciudad: "Calingasta", postalCode: "5401" },
+  { ciudad: "Jáchal", postalCode: "5460" },
+  { ciudad: "Rodeo", postalCode: "5449" },
+  { ciudad: "San Juan", postalCode: "5400" },
+  { ciudad: "Valle Fértil", postalCode: "5447" },
+];
+
+const sanLuis = [
+  { ciudad: "Merlo", postalCode: "5881" },
+  { ciudad: "Quines", postalCode: "5885" },
+  { ciudad: "San Luis", postalCode: "5700" },
+  { ciudad: "Villa Mercedes", postalCode: "5730" },
+];
+
+const santaCruz = [
+  { ciudad: "Las Heras", postalCode: "9017" },
+  { ciudad: "Pico Truncado", postalCode: "9043" },
+];
+
+const santiagoDelEstero = [
+  { ciudad: "Bandera", postalCode: "3624" },
+  { ciudad: "Santiago del Estero", postalCode: "4200" },
+  { ciudad: "Termas", postalCode: "4220" },
+];
+
+const tierraDelFuego = [{ ciudad: "Ushuaia", postalCode: "9410" }];
+
+// fin objetos lugares de atención -----------
